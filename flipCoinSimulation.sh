bf76f2db4f1e52f1afd8523c867a933d87574d02
purushothaman-source <purushoth99jun@gmail.com>
2020-11-18 18:25:25 +0530

    Added:singlet percentage

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
+declare -A Head
+declare -A Tail
+i=0
+num=0
+heads=0
+tails=0
+while (( num<50))
+do
 random=$((RANDOM%2))
 if(( $random == 1))
 then
-  echo "head"
+  Head[$heads]="head"
+   ((heads++))
+  ((num++))
 else
-  echo "tail"
+  Tail[$tails]="tail"
+  ((tails++))
+  ((num++))
 fi
+done
+
+headpercentage=$(echo $heads 50 100 | awk '{ print $1/$2*$3 }')
+echo "head Percentage: $headpercentage%"
+tailpercentage=$(echo $tails 50 100 | awk '{ print $1/$2*$3 }')
+echo "tail Percentage: $tailpercentage%"
+
+
