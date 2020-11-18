b190aa22ea5ffad8093a488553d96b170b161e13
purushothaman-source <purushoth99jun@gmail.com>
2020-11-18 18:56:21 +0530

    Added:Doublet

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,28 +1,56 @@
 #!/bin/bash
-declare -A Head
-declare -A Tail
-i=0
+declare -A HH
+declare -A TT
+declare -A HT
+declare -A TH
+
 num=0
-heads=0
-tails=0
+hh=0
+tt=0
+ht=0
+th=0
 while (( num<50))
 do
-random=$((RANDOM%2))
-if(( $random == 1))
+random1=$((RANDOM%2))
+random2=$((RANDOM%2))
+if (( $random1 ==1 && $random2 ==1 ))
 then
-  Head[$heads]="head"
-   ((heads++))
-  ((num++))
+
+     HH[$hh]="HH"
+     ((hh++))
+     ((num++))
+elif (( $random1 ==0 && $random2 ==0 ))
+then
+     TT[$tt]="TT"
+    ((tt++))
+    ((num++))
+elif (( $random1 ==1 && $random2 ==0 ))
+then
+     HT[$ht]="HT"
+    ((ht++))
+    ((num++))
 else
-  Tail[$tails]="tail"
-  ((tails++))
-  ((num++))
+     TH[$th]="TH"
+    ((th++))
+    ((num++))
 fi
 done
 
-headpercentage=$(echo $heads 50 100 | awk '{ print $1/$2*$3 }')
-echo "head Percentage: $headpercentage%"
-tailpercentage=$(echo $tails 50 100 | awk '{ print $1/$2*$3 }')
-echo "tail Percentage: $tailpercentage%"
+HHlength=${#HH[@]}
+echo "HH :${#HH[@]}"
+TTlength=${#TT[@]}
+echo "TT :${#TT[@]}"
+HTlength=${#HT[@]}
+echo "HT :${#HT[@]}"
+THlength=${#TH[@]}
+echo "TH :${#TH[@]}"
 
+HHpercentage=$(echo $HHlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "HH Percentage: $HHpercentage%"
+TTpercentage=$(echo $TTlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "TT Percentage: $TTpercentage%"
+HTpercentage=$(echo $HTlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "HT Percentage: $HTpercentage%"
+THpercentage=$(echo $THlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "TH Percentage: $THpercentage%"
 
