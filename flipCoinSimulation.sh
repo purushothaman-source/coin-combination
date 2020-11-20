9e7323f59e4f985b6100ed1587bfaf3d79578f43
purushothaman-source <purushoth99jun@gmail.com>
2020-11-20 19:02:25 +0530

    Added :combination of singlet,doublet,triplet

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -8,7 +8,22 @@ declare -A TTH
 declare -A HTT
 declare -A THH
 
+declare -A HH
+declare -A TT
+declare -A HT
+declare -A TH
 
+
+hh=0
+tt=0
+ht=0
+th=0
+
+declare -A head
+declare -A tail
+
+heads=0
+tails=0
 num=0
 hhh=0
 ttt=0
@@ -23,6 +38,41 @@ do
 random1=$((RANDOM%2))
 random2=$((RANDOM%2))
 random3=$((RANDOM%2))
+
+if(( $random1 == 1))
+then
+  Head[$heads]="head"
+   ((heads++))
+  ((num++))
+else
+  Tail[$tails]="tail"
+  ((tails++))
+  ((num++))
+fi
+
+if (( $random1 ==1 && $random2 ==1 ))
+then
+
+     HH[$hh]="HH"
+     ((hh++))
+     ((num++))
+elif (( $random1 ==0 && $random2 ==0 ))
+then
+     TT[$tt]="TT"
+    ((tt++))
+    ((num++))
+elif (( $random1 ==1 && $random2 ==0 ))
+then
+     HT[$ht]="HT"
+    ((ht++))
+    ((num++))
+else
+     TH[$th]="TH"
+    ((th++))
+    ((num++))
+fi
+
+
 if(( $random1 ==1 && $random2 ==1 && $random3 ==1 ))
 then
 
@@ -103,3 +153,26 @@ echo "HTT Percentage: $HTTpercentage%"
 THHpercentage=$(echo $THHlength 50 100 | awk '{ print $1/$2*$3 }')
 echo "THH Percentage: $THHpercentage%"
 
+headpercentage=$(echo $heads 50 100 | awk '{ print $1/$2*$3 }')
+echo "head Percentage: $headpercentage%"
+tailpercentage=$(echo $tails 50 100 | awk '{ print $1/$2*$3 }')
+echo "tail Percentage: $tailpercentage%"
+
+
+HHlength=${#HH[@]}
+echo "HH :${#HH[@]}"
+TTlength=${#TT[@]}
+echo "TT :${#TT[@]}"
+HTlength=${#HT[@]}
+echo "HT :${#HT[@]}"
+THlength=${#TH[@]}
+echo "TH :${#TH[@]}"
+
+HHpercentage=$(echo $HHlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "HH Percentage: $HHpercentage%"
+TTpercentage=$(echo $TTlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "TT Percentage: $TTpercentage%"
+HTpercentage=$(echo $HTlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "HT Percentage: $HTpercentage%"
+THpercentage=$(echo $THlength 50 100 | awk '{ print $1/$2*$3 }')
+echo "TH Percentage: $THpercentage%"
